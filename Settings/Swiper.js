1dd22009df9714220f51b725cf916a957a0ae524
Esteban Victorio <stephanvictory@gmail.com>
2018-12-10 21:15:17 -0800

    - Updated styles

--- Settings/Swiper.js
+++ Settings/Swiper.js
@@ -7,7 +7,7 @@ const HomePlaylist =
     effect: 'coverflow',
     grabCursor: false,
     centeredSlides: true,
-    slidesPerView: 'auto',
+    slidesPerView: 3,//'auto',
     coverflowEffect: {
       rotate: 50,
       stretch: 0,
@@ -22,8 +22,29 @@ const HomePlaylist =
       slideChange: function () {
         console.log('slidechange: ' + this.activeIndex);
       }
-    }
-  };
+    },
+    breakpoints: {
+   // when window width is <= 320px
+   240: {
+     slidesPerView: 1,
+     spaceBetween: 5
+   },
+   // when window width is <= 480px
+   360: {
+     slidesPerView: 1,
+     spaceBetween: 7
+   },
+   // when window width is <= 640px
+   480: {
+     slidesPerView: 1,
+     spaceBetween: 10
+   },
+   800: {
+     slidesPerView: 1,
+     spaceBetween: 15
+   }
+ }
+};
 
 export {
   HomePlaylist
